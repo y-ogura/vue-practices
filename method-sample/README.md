302d583d05b95597d123782eb79a4ad6bb6cd468
小倉裕一 <j211025ogu@gmail.com>
2018-10-04 09:46:15 +0900

    add method sample

---
+++ method-sample/README.md
@@ -0,0 +1,44 @@
+# method-sample
+
+> My geometric Nuxt.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm start
+
+# generate static project
+$ npm run generate
+```
+
+## Question
+### ja
+`pages/index.vue`に以下の機能を実装してください。
+- count up をクリックするとcountの数字が1ずつ増える
+- count down をクリックするとcountの数字が1ずつ減る
+- countのminは0、maxは10
+
+### en
+You should implement functions  of following conditions in `pages/index.vue`.
+
+- increment by 1 the count when you click `count up` button.
+- decrement by 1 the count when you click `count down` button
+- the count min is 0 and max is 10.
+
+## Answer
+### ja
+`pages/answer/index.vue`に解答が記載されています。
+
+### en
+You can know answer looking the `pages/answer/index.vue`.
+
+## 
+For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
---
+++ method-sample/pages/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <section class="container">
+    <div>
+      <h3>count: {{ count }}</h3>
+
+      <!-- add click method -->
+      <el-button>count down</el-button>
+      <el-button>count up</el-button>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    countUp () {
+      // write here your answer
+    },
+    countDown () {
+      // write here your answer
+    }
+  }
+}
+</script>
+
+<style>
+
+.container
+{
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.title
+{
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+.subtitle
+{
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+.links
+{
+  padding-top: 15px;
+}
+</style>
